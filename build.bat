fa095aad34ec25ee1b12399109ef362460611b58
rafft2 <rafft2-dev@pm.me>
2026-06-14 01:43:15 +0200

    write line super naive version

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 IF NOT EXIST bin/ mkdir bin
 
-set warning_opts=-DWIN32_LEAN_AND_MEAN -DDEBUG_BUILD -D_CRT_SECURE_NO_WARNINGS -Wall -WX -wd4100 -wd4820 -wd4189 -wd5045 -wd4005 -wd4668 -wd4702 -wd4201 -wd5246 -wd4191
+set warning_opts=-DWIN32_LEAN_AND_MEAN -DDEBUG_BUILD -D_CRT_SECURE_NO_WARNINGS -Wall -WX -wd4100 -wd4820 -wd4189 -wd5045 -wd4005 -wd4668 -wd4702 -wd4201 -wd5246 -wd4191 -wd4514
 
 cl -nologo -Zi -GR- -EHa- -Odi %warning_opts% /I thirdparty\stb src/sf_main.cpp -Fe:"bin/sf.exe" -Fo:"bin/sf.obj" -Fd:"bin/sf.pdb"
 
--- src/sf_main.cpp
+++ src/sf_main.cpp
@@ -42,15 +42,47 @@ static inline color_rgba ColorFromVec(vec4 v)
     return(result);
 }
 
-static inline void WritePixel(u8* image, u32 x, u32 y, u32 stride, color_rgba color)
+static inline void WritePixel(u8* image, u32 stride, u32 x, u32 y, color_rgba color)
 {
     memcpy(image + (y * stride + x)*4, color.rgba, 4);
 }
 
+struct vec2
+{
+    f32 x, y;
+};
+
+static inline vec2 PixelCenterFromCoords(u32 x, u32 y)
+{
+    vec2 result;
+    result.x = (f32)x + 0.5f;
+    result.y = (f32)y + 0.5f;
+    return(result);
+}
+
+static inline void WriteLine(u8* image, u32 stride, u32 ax, u32 ay, u32 bx, u32 by, u32 line_size, color_rgba color)
+{
+    vec2 a = PixelCenterFromCoords(ax, ay);
+    vec2 b = PixelCenterFromCoords(bx, by);
+    f32 dy = b.y - a.y;
+    f32 dx = b.x - a.x;
+    for(u32 x = ax; x <= bx; x++)
+    {
+        f32 y = ((f32)x - (f32)bx) * dy / dx + (f32)by;
+        for(u32 i = 0; i < line_size; i++)
+        {
+            for(u32 j = 0; j < line_size; j++)
+            {
+                WritePixel(image, stride, x - line_size + i, (u32)y - line_size + j, color);
+            }
+        }
+    }
+}
+
 int main(void)
 {
-    u32 image_width = 640;
-    u32 image_height = 480;
+    u32 image_width = 64;
+    u32 image_height = 64;
     u32 bytes_per_pixel = 4;
     u8 *image_data = (u8*)malloc(image_width*image_height*4);
     for(u32 y = 0; y < image_height; y++)
@@ -60,9 +92,13 @@ int main(void)
             f32 py = (f32)((f32)y / (f32)image_height);
             f32 px = (f32)((f32)x / (f32)image_width); 
             vec4 color = {1.0f - py, px, py, 255.0f};
-            WritePixel(image_data, x, y, image_width, ColorFromVec(color));
+            WritePixel(image_data, image_width, x, y, ColorFromVec(color));
         }
     }
+    vec4 color = {0.0f, 0.0f, 0.0f, 1.0f};
+    WriteLine(image_data, image_width, 4, 4, 20, 50, 3, ColorFromVec(color));
+    WriteLine(image_data, image_width, 32, 32, 56, 10, 3, ColorFromVec(color));
+    stbi_flip_vertically_on_write(true);
     stbi_write_png("out.png", (s32)image_width, (s32)image_height, (s32)bytes_per_pixel, image_data, (s32)(image_width * bytes_per_pixel));
 
     exit(0);
